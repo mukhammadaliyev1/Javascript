0378dd3e36003af8c28bd82c26a147b90f5cad8e
mukhammadaliyev1 <muhammadnurmamadaliyev335@gmail.com>
2024-05-31 17:08:15 +0500

    tugad

---
+++ js/index.js
@@ -0,0 +1,251 @@
+
+// 1 - masala
+// let a =  +prompt("a ning qiymatini kiriting");
+// let P = 4 * a;
+// console.log  +prompt( " a ning perimetri " + P + " ga teng");
+
+
+
+
+// 2-masala
+
+// let a = +prompt("kvadratining tomonining qiymatini kiriting");
+// let s= a*a
+// console.log("kvadratining yuzasi " + s + " ga teng");
+
+
+
+// 3-masala
+
+// let a= +prompt("a ning qiymatini kiriting");
+// let b= +prompt("b ning qiymatini kiriting");
+// let P = (a+b)*2;
+// let S= a*b;
+// console.log(P);
+// console.log(S);
+
+
+
+// 4-masala;
+
+// let d= +prompt("aylananing diametrini  kiriting");
+// let L= Math.PI*d;
+// console.log +prompt("aylananing uzunligi " + L + " ga teng");
+
+
+
+//  5-misol
+
+// let a= +prompt("a ning qiymatini kiriting");
+// let V= a*a*a;
+// let S= 6*a*a;
+// console.log("aylananing hajmi " + V + " ga teng");
+// console.log("aylananing yuzasi " + S + " ga teng");
+
+
+
+// // 6-misol
+
+// let a= +prompt("a ning qiymatini kiriting");
+// let b= +prompt("b ning qiymatini kiriting");
+// let c= +prompt("c ning qiymatini kiriting");
+// let V= a*b*c;
+// let S= 2*(a*b+a*c+b*c);
+// console.log("aylananing hajmi " + V + " ga teng");
+// console.log("aylananing yuzasi " + S + " ga teng");
+
+// 7-misol
+
+
+// let R= +prompt("R ning qiymatini kiriting");
+// let L = 2*Math.PI*R;
+// let S = Math.PI*R*R;
+// console.log("aylananing uzunligi " + L + " ga teng");
+// console.log("aylananing yuzasi " + S + " ga teng");
+
+//  8-misol
+
+
+
+// let a= +prompt("1 sonning qiymatini kiriting");
+// let b= +prompt("2 sonning qiymatini kiriting");
+// let c= (a+b)/2;
+// console.log("1 sonning va 2 sonning o'rta arifmetig"  + c +  " ga teng");
+
+// 9-misol
+ 
+
+// let R1= +prompt("R1ning qiymatini kiriting");
+// let R2= +prompt("R2ning qiymatini kiriting");
+// let S1= Math.PI*R1;
+// let S2= Math.PI*R2;
+// let S3= Math.PI*(R1-R2);
+// console.log("1-aylananing yuzasi " + S1 + " ga teng");
+// console.log("2-aylananing yuzasi " + S2 + " ga teng");
+// console.log("aylanalararo yuzasi " + S3 + " ga teng");
+
+
+
+// 10 -misol
+
+// let Tf= +prompt("Tf ning qiymatini kiriting");
+// let Tc= (Tf-32)*5/9;
+// console.log("Farengeydan Celciyga o'zgaruvchi " + Tc + " ga teng");
+
+
+
+// 11-misol
+
+
+// let L=+prompt("L ning qiymatini kiriting");
+// let m= L/100;
+// console.log(" " + m + " metr ga teng");
+
+
+//12-misol
+
+
+// let k=+prompt("k ning qiymatini kiriting");
+// let t=k/1000;
+// console.log(" " + t + " tonnaga teng");
+
+
+// 13-misol
+
+
+
+// let b= +prompt("b ning qiymatini kiriting");
+// let kb=b/1024;
+// console.log(" " + kb + " kilobaytga teng");
+
+
+// 14-misol
+
+
+
+// let A= +prompt("A ning qiymatini kiriting");
+// let B= +prompt("B ning qiymatini kiriting");
+// let C=A/B;
+// console.log("A ning ichida B ni " + C + "  marta joylashtirish mumkin ");
+
+
+
+
+
+// 15-misol
+
+
+
+// let a= +prompt("a ni kiriting");
+// let b= +prompt("b ni kiriting");
+// let c= a%b;
+// console.log("a ning ichida b ni joylashmagan qoldigi " + c + " ga teng");
+
+
+
+
+
+// 16-misol
+
+// let a= +prompt("ikki xonali sonni kiriting");
+// let b= a%10;
+// let c= a/10;
+// console.log("ikki xonali sonning 10 liklar xonasi " + c + " ga teng");
+// console.log("ikki xonali sonning 1 liklar xonasi " + b + " ga teng");
+
+
+
+// 17-misol
+
+
+
+
+
+// let a= +prompt("ikki xonali sonni kiriting");
+//  let b= a%10;
+//  let c= a/10;
+//  let d=a%10+a/10;
+//  console.log("ikki xonali sonning raqamlari yigindisi " + d + " ga teng");
+
+
+
+
+
+
+
+
+
+
+
+// 18-misol
+
+
+
+
+// let a= +prompt("ikki xonali sonni kiriting");
+
+
+//  let b= a%10;
+//  let c= a/10;
+//  console.log(b,c);
+
+
+
+// 19-misol
+
+
+// let a= +prompt("uch xonali sonni kiriting");
+
+// let b=a/100;
+// console.log( "uch xonali sonning 100 liklar xonasi " + b + " ga teng");
+
+// 20-misol
+
+
+// let a= +prompt("uch xonali sonni kiriting");
+// let b=a%100/10;
+// let c= a%100%10;
+// console.log( c, b);
+
+
+
+// // 21-misol
+// let a= +prompt("uch xonali sonni kiriting");
+
+// let b=a%100/10;
+
+// let c= a%100%10;
+
+// let d=a/100;
+// let e=d=a/100+a%100%10+a%100/10;
+// console.log(e);
+
+
+
+// 22-misol
+
+
+// let a= +prompt("uch xonali sonni kiriting");
+
+// let b=a%100/10;
+
+// let c= a%100%10;
+
+//  let d=a/100;
+//  console.log( c,b,d);
+
+
+
+//23-misol
+
+
+// let a= +prompt("uch xonali sonni kiriting");
+
+// let d=a/100;
+
+// let b=a%100/10;
+
+// let c= a%100%10;
+// console.log( b,d,c);
+
+
